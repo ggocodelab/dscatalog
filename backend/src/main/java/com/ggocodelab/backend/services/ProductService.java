530951caf26e4901b574342e1345939b87a60601
ggocodelab <brgillian@gmail.com>
2026-01-21 11:10:20 -0300

    feat(product): add find by id endpoint

--- backend/src/main/java/com/ggocodelab/backend/services/ProductService.java
+++ backend/src/main/java/com/ggocodelab/backend/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.ggocodelab.backend.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ggocodelab.backend.dtos.ProductDTO;
 import com.ggocodelab.backend.entities.Product;
+import com.ggocodelab.backend.exceptions.ResourceNotFoundException;
 import com.ggocodelab.backend.repositories.ProductRepository;
 
 @Service
@@ -21,7 +24,12 @@ public class ProductService {
 		Page<Product> list = repository.findAll(pageRequest);		
 		return list.map(x -> new ProductDTO(x));		
 	}
-
 	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ProductDTO(entity);
+	}
 
 }
